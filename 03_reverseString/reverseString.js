b3056097e7698e03136283050ee320755fa2ca6d
ansiblejane <33788618+ansiblejane@users.noreply.github.com>
2023-08-15 20:45:27 -0700

    Finished Exercise 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    finalString = '';
 
+    for (let index = (string.length - 1); index > -1; index--) {
+        finalString = finalString.concat(string[index]);
+    }
+
+    return finalString;
 };
 
 // Do not edit below this line
