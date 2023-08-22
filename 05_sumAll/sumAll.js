9ed3e9677395cad050196bbbf5367b20632d94d1
ansiblejane <33788618+ansiblejane@users.noreply.github.com>
2023-08-22 15:44:04 -0700

    Finished Exercise 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,22 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    let finalSum = 0; 
 
+    if (Number.isInteger(num1) && Number.isInteger(num2) && num1 > 0 && num2 > 0){
+        if (num2 > num1){
+            for (let num = num1; num <= num2; num++) {
+                finalSum += num;
+            }
+        } else {
+            for (let num = num2; num <= num1; num++) {
+                finalSum += num;
+            }          
+        }
+    } else {
+        return "ERROR";
+    }
+
+
+    return finalSum;
 };
 
 // Do not edit below this line
