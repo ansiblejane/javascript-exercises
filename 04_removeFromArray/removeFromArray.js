0d69f82e523cedef88f01e564e43635c21549f06
ansiblejane <33788618+ansiblejane@users.noreply.github.com>
2023-08-22 15:26:59 -0700

    Finished Exercise04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,18 @@
-const removeFromArray = function() {
+const removeFromArray = function(initialArray, ...argsToRemove) {
 
+    // for item in args to Remove, see if item is in initialArray; if item is in initialArray, splice it out
+
+    for (let i = 0; i < argsToRemove.length; i++) {
+        for (let j = 0; j < initialArray.length;) {
+            if (argsToRemove[i] === initialArray[j]) {
+                initialArray.splice(j, 1);
+            } else {
+                j++; 
+            }
+        }
+    }
+
+    return initialArray; 
 };
 
 // Do not edit below this line
